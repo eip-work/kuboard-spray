3b9d78e4512c48cd2ef6b160bcf103cfb5c33a35
Shao Huan Qing <shaohq@foxmail.com>
2021-12-14 21:24:19 +0800

    init logrus

--- server/kuboard-spray.go
+++ server/kuboard-spray.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/eip-work/kuboard-spray/api/cluster"
 	"github.com/eip-work/kuboard-spray/api/private_key"
 	"github.com/eip-work/kuboard-spray/api/resource"
+	"github.com/eip-work/kuboard-spray/log"
 	"github.com/eip-work/kuboard-spray/login"
 	"github.com/eip-work/kuboard-spray/vue"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func setupRouter() *gin.Engine {
@@ -20,6 +25,7 @@ func setupRouter() *gin.Engine {
 	api.GET("/clusters", cluster.ListClusters)
 	api.POST("/clusters", cluster.CreateCluster)
 	api.GET("/clusters/:cluster", cluster.GetCluster)
+	api.PUT("/clusters/:cluster", cluster.ModifyCluster)
 
 	api.GET("/clusters/:cluster/private-keys", private_key.ListPrivateKey)
 	api.GET("/clusters/:cluster/private-keys/:name", private_key.GetPrivateKey)
@@ -35,7 +41,33 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+
+	initLogrus()
+
 	router := setupRouter()
 
 	router.Run(":8006")
+}
+
+func initLogrus() {
+	logrus.SetFormatter(new(log.KuboardLogFormatter))
+	// logrus.SetFormatter(&logrus.TextFormatter{})
+
+	// logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetReportCaller(true)
+	logrus.SetOutput(os.Stdout)
+
+	value := os.Getenv("KUBOARD_SERVER_LOGRUS_LEVEL")
+	if value == "" {
+		value = "trace"
+	}
+	level, err := logrus.ParseLevel(value)
+	if err == nil {
+		fmt.Println("设置日志级别为 " + value)
+		logrus.SetLevel(level)
+	} else {
+		fmt.Println("请检查 KUBOARD_SERVER_LOGRUS_LEVEL 的值，可选的有 panic / fatal / error / warn / info / debug / trace ，当前为： " + value)
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
 }

f5a73e92fca469e970f2cc58d8574132c574ee2d
Shao Huan Qing <shaohq@foxmail.com>
2021-12-19 13:00:35 +0800

    安装时可以显示详细信息

--- server/api/cluster/install_cluster.go
+++ server/api/cluster/install_cluster.go
@@ -78,7 +78,8 @@ func InstallCluster(c *gin.Context) {
 
 	// end merge resourcePackage info into inventory
 
-	common.MapSet(inventory, "all.vars.download_keep_remote_cache", true)
+	// common.MapSet(inventory, "all.vars.download_keep_remote_cache", true)
+	common.MapSet(inventory, "all.vars.kuboardspray_no_log", !req.Verbose)
 	common.MapSet(inventory, "all.vars.download_run_once", true)
 	common.MapSet(inventory, "all.vars.download_localhost", true)
 	common.MapSet(inventory, "all.vars.download_always_pull", false)
--- web/src/views/clusters/ClusterProcessing.vue
+++ web/src/views/clusters/ClusterProcessing.vue
@@ -7,6 +7,9 @@ en:
   viewLogs: View Task Logs
   taskFinished: Task is already completed
   viewPlan: I just want to view the Cluster Parameters.
+  verbose: Trace in details
+  verbose_true: May include sensitive data in the trace, e.g. path to files, user name, password.
+  verbose_false: Some information is hidden when there is a exception, which makes it more difficult to fix the issue.
 zh:
   apply: 执 行
   confirmToApply: 将执行集群安装动作，请确认已完成集群规划！
@@ -15,6 +18,9 @@ zh:
   viewLogs: 查看任务日志
   taskFinished: 任务已结束
   viewPlan: 我想看看集群规划
+  verbose: 显示详尽的日志信息
+  verbose_true: 日志中会包含部分敏感信息，例如：文件路径、用户名密码等
+  verbose_false: 部分错误信息不能完整展示，使得出错时排查问题更困难
 </i18n>
 
 <template>
@@ -30,12 +36,26 @@ zh:
       </template>
     </el-dialog>
   </div>
-  <el-popconfirm v-else-if="!isInstalled" :confirm-button-text="$t('msg.ok')" :cancel-button-text="$t('msg.cancel')" placement="bottom-start"
-    icon="el-icon-lightning" icon-color="red" :title="$t('confirmToApply')" @confirm="applyPlan">
+  <el-popover v-else-if="!isInstalled" v-model:visible="showConfirm" placement="right-start" width="270" trigger="manual">
     <template #reference>
-      <el-button type="danger" icon="el-icon-lightning">{{$t('apply')}}</el-button>
+      <el-button type="danger" icon="el-icon-lightning" @click="showConfirm = !showConfirm">{{$t('apply')}}</el-button>
     </template>
-  </el-popconfirm>
+    <el-form @submit.prevent.stop>
+      <div style="height: 10px;"></div>
+      <el-alert type="error" style="margin-bottom: 10px;" :closable="false">
+        <i class="el-icon-lightning" style="font-size: 16px; color: red; margin-right: 10px;"></i>
+        <span class="confirmText">{{$t('confirmToApply')}}</span>
+      </el-alert>
+      <el-form-item :label="$t('verbose')">
+        <el-switch v-model="form.verbose"></el-switch>
+        <div style="width: 240px; font-size: 12px;">{{$t('verbose_' + form.verbose)}}</div>
+      </el-form-item>
+      <div style="text-align: right; margin-top: 20px;">
+        <el-button type="default" icon="el-icon-close" @click="showConfirm = false">{{$t('msg.cancel')}}</el-button>
+        <el-button type="primary" icon="el-icon-lightning" @click="applyPlan">{{$t('msg.ok')}}</el-button>
+      </div>
+    </el-form>
+  </el-popover>
 </template>
 
 <script>
@@ -47,7 +67,11 @@ export default {
   },
   data() {
     return {
-      forceHide: false
+      forceHide: false,
+      showConfirm: false,
+      form: {
+        verbose: false,
+      }
     }
   },
   computed: {
@@ -65,6 +89,7 @@ export default {
   watch: {
     'loading': function() {
       this.forceHide = false
+      this.showConfirm = false
     }
   },
   components: { },
@@ -73,7 +98,9 @@ export default {
   },
   methods: {
     applyPlan () {
-      this.kuboardSprayApi.post(`/clusters/${this.name}/install`).then(resp => {
+      this.forceHide = false
+      this.showConfirm = false
+      this.kuboardSprayApi.post(`/clusters/${this.name}/install`, this.form).then(resp => {
         this.$emit('refresh')
         this.viewTaskLogs(resp.data.data.pid)
       }).catch(e => {
@@ -88,5 +115,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.confirmText {
+  font-size: 15px;
+  color: $--color-danger;
+  font-weight: bold;
+}
 </style>

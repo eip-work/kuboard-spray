b676ece243e07fdbd5d6944400e95fbb0eedf70f
Shao Huan Qing <shaohq@foxmail.com>
2021-12-24 19:49:48 +0800

    ConfigSection

--- web/src/components/ConfigSection.vue
+++ web/src/components/ConfigSection.vue
@@ -1,23 +1,44 @@
 <i18n>
 en:
-  hi: kuboard
+  clear1: This is going to clear 
+  clear2: ', do you confirm to continue?'
 zh:
-  hi: kuboard
+  clear1: 此操作将清除
+  clear2: 的相关设置，是否继续？
+
 </i18n>
 
 <template>
   <el-form-item label-width="80px" class="config_section_section">
     <template #label>
-      <el-checkbox v-model="enabledRef" :disabled="disabled"
-        ><span class="enableButton">{{label}}</span>
-      </el-checkbox>
+      <span v-if="editMode === 'view'" class="viewLabel">{{label}}</span>
+      <template v-else>
+        <el-checkbox v-model="enabledRef" :disabled="disabled"
+          ><span class="enableButton">{{label}}</span>
+        </el-checkbox>
+        <el-popover placement="bottom-start" width="320" trigger="manual"
+          v-model:visible="visiblePopover">
+          <p>
+            {{ $t('clear1') }}
+            <i class="strong-text">{{label}}</i>
+            {{ $t('clear2') }}
+          </p>
+          <div style="text-align: right; margin: 0">
+            <el-button size="mini" type="info" plain @click="confirm(false)" icon="el-icon-close">取 消</el-button>
+            <el-button type="primary" size="mini" @click="confirm(true)" icon="el-icon-check">确 定</el-button>
+          </div>
+          <template #reference>
+            <div></div>
+          </template>
+        </el-popover>
+      </template>
     </template>
     <div>
       <div :class="expandedRef ? 'config_section_header expanded' : 'config_section_header'" @click="expandedRef = !expandedRef">
         <el-icon style="vertical-align: middle;" v-if="expandedRef"><arrow-down-bold /></el-icon>
         <el-icon style="vertical-align: middle;" v-else><arrow-up-bold /></el-icon>
         <span style="margin-left: 5px">
-          {{ description || label }}
+          {{ description || label }} {{editMode}}
         </span>
       </div>
       <el-collapse-transition>
@@ -41,17 +62,23 @@ export default {
   },
   data () {
     return {
-      expanded: true
+      expanded: true,
+      visiblePopover: false,
     }
   },
+  inject: ['editMode'],
   computed: {
     enabledRef: {
       get () {
         return this.enabled
       },
       set (v) {
-        this.expanded = v
-        this.$emit('update:enabled', v)
+        if (v === false) {
+          this.visiblePopover = true
+        } else {
+          this.expanded = v
+          this.$emit('update:enabled', v)
+        }
       }
     },
     expandedRef: {
@@ -67,7 +94,15 @@ export default {
   mounted () {
   },
   methods: {
-
+    confirm (flag) {
+      if (flag) {
+        this.expanded = false
+        this.$emit('update:enabled', false)
+      } else {
+        this.expanded = true
+      }
+      this.visiblePopover = false
+    }
   }
 }
 </script>
@@ -100,5 +135,8 @@ export default {
   background-color: white;
   border: solid 1px $--color-primary-light-9;
   border-top: none;
+}
+.viewLabel {
+  font-weight: bold;
 }
 </style>

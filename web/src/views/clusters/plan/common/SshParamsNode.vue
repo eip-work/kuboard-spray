17ef35f99d942792ac6e84da9798646afce56cf9
Shao Huan Qing <shaohq@foxmail.com>
2022-01-04 23:30:13 +0800

    安装成功后，仍然可以修改节点 ip （待进一步优化）

--- web/src/views/clusters/plan/common/SshParamsNode.vue
+++ web/src/views/clusters/plan/common/SshParamsNode.vue
@@ -10,7 +10,7 @@ zh:
 
 <template>
   <ConfigSection v-model:enabled="enableSsh" label="SSH" :description="description" disabled anti-freeze>
-    <FieldString :holder="holder" fieldName="ansible_host" :prop="`all.hosts.${nodeName}`"
+    <FieldString :holder="holder" fieldName="ansible_host" :prop="`all.hosts.${nodeName}`" anti-freeze
       :placeholder="$t('ansible_host_placeholder')" :required="isNode"></FieldString>
     <FieldString :holder="holder" fieldName="ansible_port" :prop="`all.hosts.${nodeName}`"
       :placeholder="placeholder('ansible_port')" anti-freeze

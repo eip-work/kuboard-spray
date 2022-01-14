a5235d048c6c2ab9b6c799bfe566d0dc3299e57b
Shao Huan Qing <shaohq@foxmail.com>
2022-01-15 00:14:18 +0800

    timeout

--- server/api/cluster/state/ping_pong.go
+++ server/api/cluster/state/ping_pong.go
@@ -56,7 +56,7 @@ func PingPong(clusterName string, nodes string) (*PingPongResult, error) {
 			"-m", "ping",
 			"-i", inventoryYamlPath,
 			"--fork", "100",
-			"-t", "2",
+			"--timeout", "20",
 			"-e", "kuboardspray_cluster_dir=" + constants.GET_DATA_DIR() + "/cluster/" + clusterName,
 		},
 		Env:     []string{"ANSIBLE_CONFIG=" + constants.GET_ADHOC_CFG_PATH()},

0f46f6a71ad434d982e3b0a6f8f0350e12d6d599
Shao Huan Qing <shaohq@foxmail.com>
2022-02-25 19:47:12 +0800

    添加节点时，提示对节点的要求

--- web/src/views/clusters/plan/common/AddNode.vue
+++ web/src/views/clusters/plan/common/AddNode.vue
@@ -7,6 +7,9 @@ en:
   removeNodeFirst: Please remove node or cancel removing node first.
   invalidName: Hostname must consist of lower case alphanumeric characters or '-', and must start with an alphanumeric character
   cannotUseKeyword: Cannot use {keyword} as node name.
+
+  nodeRequirement1: The machine on which KuboardSpray runs cannot be used as a k8s node.
+  nodeRequirement2: Please make sure to use a clean machine as a node (haven't tried to install k8s on it, and no other programs run on it).
 zh:
   addNode: 添加节点
   nodeName: 节点名称
@@ -15,11 +18,14 @@ zh:
   removeNodeFirst: 请先删除或者取消删除节点
   invalidName: 必须由小写字母、数字、减号组成，且必须以字母开头，以字母/数字结尾
   cannotUseKeyword: 不能使用关键字 {keyword} 作为节点名称
+
+  nodeRequirement1: KuboardSpray 所在机器不能用作 k8s 节点
+  nodeRequirement2: 请确保您的节点服务器是一个干净的机器（没有安装过 k8s，不用来运行其他程序）
 </i18n>
 
 <template>
   <el-popover placement="right-start" :title="$t('addNode')" v-if="editMode !== 'view'"
-    v-model:visible="visible" :width="420" trigger="manual">
+    v-model:visible="visible" :width="480" trigger="manual">
     <template #reference>
       <el-button icon="el-icon-plus" type="primary" @click="visible = true"
         :disabled="editMode === 'view'">{{$t('addNode')}}</el-button>
@@ -35,6 +41,10 @@ zh:
           <el-checkbox label="kube_node">{{$t('node.kube_node')}}</el-checkbox>
         </el-checkbox-group>
       </el-form-item>
+      <div class="app_margin_bottom">
+        <li class="desc">{{ $t('nodeRequirement1') }}</li>
+        <li class="desc">{{ $t('nodeRequirement2') }}</li>
+      </div>
     </el-form>
     <div style="text-align: right;">
       <KuboardSprayLink href="https://kuboard-spray.cn/guide/maintain/add-replace-node.html" style="float: left;"></KuboardSprayLink>
@@ -130,5 +140,9 @@ export default {
 </script>
 
 <style>
-
+.desc {
+  color: var(--el-text-color-secondary);
+  font-size: 12px;
+  line-height: 24px;
+}
 </style>

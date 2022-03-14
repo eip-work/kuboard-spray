9c093760663e3bbb7d642374adf4859e71838829
Shao Huan Qing <shaohq@foxmail.com>
2022-03-14 20:37:06 +0800

    优化 websocket 相关错误的提示

--- web/src/views/logs/K8sTerminalErrorHint.vue
+++ web/src/views/logs/K8sTerminalErrorHint.vue
@@ -1,19 +1,20 @@
 <i18n>
 en:
-  "referto": "If you are using Reverse Proxy, please refer to"
-  "commonProblems": "Recommended Reverse Proxy configuration."
+  error: "Cannot access the following url via websocket protocol: "
+  "commonProblems": "Refer to help on kuboard.cn"
   "confirm": "Do you confirm to retry ?"
 zh:
-  "referto": "如果您设置了反向代理，请参考"
-  "commonProblems": "Kuboard 推荐的反向代理配置。"
+  error: 不能通过 websocket 协议访问如下地址：
+  "commonProblems": "查看问题原因及解决办法"
   "confirm": "确认重试？"
 </i18n>
 
 <template>
   <div style="text-align: left;">
     <el-dialog :title="$t('msg.prompt')" v-model="dialogVisible" width="60%" :before-close="handleClose">
-      <p>{{$t('referto')}}</p>
-      <KuboardSprayLink href="https://kuboard-spray.cn/guide/extra/websocket.html" :size="14">查看如何配置反向代理</KuboardSprayLink>
+      <p>{{ $t('error') }}</p>
+      <p class="app_text_mono" style="font-size: 12px; margin-left: 20px; margin-bottom: 20px;">{{message}}</p>
+      <KuboardSprayLink href="https://kuboard-spray.cn/guide/extra/websocket.html" :size="14" type="danger">{{$t('commonProblems')}}</KuboardSprayLink>
     </el-dialog>
   </div>
 </template>
